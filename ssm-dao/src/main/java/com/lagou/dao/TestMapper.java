0a8bc68046892676af53cae1c313f6d3e4f5f7e6
weishuai <2576297929@qq.com>
2022-04-22 15:44:35 +0800

    add GitMapper

--- ssm-dao/src/main/java/com/lagou/dao/TestMapper.java
+++ ssm-dao/src/main/java/com/lagou/dao/TestMapper.java
@@ -7,4 +7,12 @@ import java.util.List;
 public interface TestMapper {
     //查询所有用户
     public  List<Test> findAll();
+    public  List<Test> findAll2();
+    public  List<Test> findAll3();
+    public  List<Test> findAll4();
+    public  List<Test> findAll5();
+    public  List<Test> findAll6();
+    public  List<Test> findAll7();
+    public  List<Test> findAll8();
+
 }
