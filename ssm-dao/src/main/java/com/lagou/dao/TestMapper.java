bc4a4024a14b4e46650429fdbf7c92cc86297e44
weishuai0421 <104176403+weishuai0421@users.noreply.github.com>
2022-04-22 15:44:04 +0800

    Update TestMapper.java

--- ssm-dao/src/main/java/com/lagou/dao/TestMapper.java
+++ ssm-dao/src/main/java/com/lagou/dao/TestMapper.java
@@ -6,5 +6,12 @@ import java.util.List;
 
 public interface TestMapper {
     //查询所有用户
-    public  List<Test> findAll();
+    public  List<Test> findAll1();
+    public  List<Test> findAll12();
+    public  List<Test> findAll13();
+    public  List<Test> findAll14();
+    public  List<Test> findAll15();
+    public  List<Test> findAll16();
+    public  List<Test> findAll17();
+    public  List<Test> findAll18();
 }
